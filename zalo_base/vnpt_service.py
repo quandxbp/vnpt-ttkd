b24426d8ed1f20f7a6b6de0bd5eca78f4b4538b3
Quan Bui <quandxbp@gmail.com>
2021-10-04 16:59:51 +0700

    [FIX] vnpt token bugs

--- zalo_base/vnpt_service.py
+++ zalo_base/vnpt_service.py
@@ -1,18 +1,39 @@
 import requests
+from .utils import *
+
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 class VnptService:
 
     def __init__(self):
         self.root_path = "http://192.168.110.66:8086"
-        self.access_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6IjEyMzQiLCJuYmYiOjE2MzIxMTk5MjksImV4cCI6MTYzMjcyNDcyOSwiaWF0IjoxNjMyMTE5OTI5fQ.2xEJGOCMUANxLo5nKDrtSQW1v8QLtEuKDHGR3a85A4Q"
         self.headers = self.get_headers()
-
 
     def get_headers(self):
         return {
             'Content-Type': 'application/json',
-            'Authorization': f'Bearer {self.access_token}'
+            'Authorization': f'Bearer {self.get_access_token_from_file()}'
         }
+
+    def get_access_token_from_file(self):
+        data = read_json(BASE_DIR / 'config.json')
+        return data.get('vnpt_access_token', False)\
+    
+    def set_access_token_from_file(self, access_token):
+        data = read_json(BASE_DIR / 'config.json')
+        data['vnpt_access_token'] = access_token
+        store_json(BASE_DIR / 'config.json', data)
+
+    def get_access_token(self):
+        url = 'http://192.168.110.66:8086/api/nguoi-dung/login?username=admin&password=admin'
+        response = requests.post(url)
+        if response.status_code == 200:
+            json_res = response.json()
+            if json_res.get('success') and json_res.get('data'):
+                return self.set_access_token_from_file(json_res['data']['token'])
 
     def get_payment_debt(self, zalo_id):
         url = f'{self.root_path}/api/zalo-oa/tra-cuu-thong-tin-no'
@@ -28,6 +49,14 @@ class VnptService:
                     'success': 1,
                     'data': json_res['data']
                 }
+            else:
+                return {
+                    'success': 0,
+                    'message': message
+                }
+        elif response.status_code == 401:
+            self.get_access_token()
+            return self.get_payment_debt(zalo_id)
         return {
             'success': 0,
             'message': message
