78df9b5305ce820a072774f2485c31826310cbca
Quan Bui <quandxbp@gmail.com>
2021-12-03 13:47:07 +0700

    [FIX] bugs

--- ioffice/utils.py
+++ ioffice/utils.py
@@ -6,7 +6,7 @@ def store_json(file_name, data):
         json.dump(data, f, ensure_ascii=False, indent=4)
 
 def read_json(file_name):
-    with open(file_name) as json_file:
+    with open(file_name, encoding="utf8") as json_file:
         return json.load(json_file)
 
 patterns = {
